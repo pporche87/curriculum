d3d6dcf7b71fc76f6305777b8d35220830f16841
Jared Grippe <jared@deadlyicon.com>
2017-09-08 15:52:10 -0700

    WIP

--- web-server/routes/admin.js
+++ web-server/routes/admin.js
@@ -6,4 +6,21 @@ module.exports = app => {
     response.render('admin/index')
   })
 
+  app.get('/admin/users', (request, response, next) => {
+    request.backOffice.getAllLearners()
+      .then(users => {
+        response.render('admin/users/index', { users })
+      })
+      .catch(next)
+  })
+
+  app.get('/admin/users/:handle', (request, response, next) => {
+    request.backOffice.getLearnerByHandle(request.params.handle)
+      .then(user => {
+        response.render('admin/users/show', { user })
+      })
+      .catch(next)
+
+  })
+
 }
